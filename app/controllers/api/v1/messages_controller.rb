5a74bd41ed6bc32557c0b1cc709a4aa08315ffa0
mryeminthu <dhammaarchive@hotmail.com>
2024-02-12 12:12:05 +0630

    Update readme file

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
     def greeting
-      render json: { greeting: Message.order('RANDOM()').first&.content }
+      render json: Message.order('RANDOM()').first&.content
     end
   end
   
