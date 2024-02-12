48e6eacaf9c3df5fdbeb0d70f77285c4e3c78dba
mryeminthu <dhammaarchive@hotmail.com>
2024-02-12 11:46:00 +0630

    Create route for random messages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :api do
+    namespace :v1 do
+      get 'messages/greeting', to: 'messages#greeting'
+    end
+  end
   # Defines the root path route ("/")
   root 'root#index'
 end
